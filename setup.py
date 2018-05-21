b88919535788bd42da040e298399bbdfb85c02bc
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-21 15:51:52 -0300

    "0.1.3"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='python-elevate',
     packages=['elevate'],
-    version='0.1.2',
+    version='0.1.3',
     description='A python utility to help manage your Ubuntu OS!',
     author='Jacob Smith',
     author_email='jacob.smith@unb.ca',
